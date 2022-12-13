429532df40ed6ab8bb8297fbc9dfcf810d2f5ebd
gagancr <120467252+gagancr@users.noreply.github.com>
2022-12-13 20:51:38 +0530

    Update curconverter.js

--- curconverter.js
+++ curconverter.js
@@ -1,11 +1,24 @@
 function onClick(){
     const cur=parseFloat(document.getElementById("textCurrency").value);
-    const conv=document.getElementById("conver").value;
+    const conv=document.getElementById("convert").value;
     let res=0.0;
     switch(conv){
-        case "dollor &#36;":
-            res=cur*2;
+        case "dollor":
+            res=cur*0.012;
             break;
+        case "austral":
+            res=cur*0.01764;
+            break;
+        
+        case "euro" :
+            res=cur*0.01140;
+            break;
+        case "franc" :
+            res=cur*7.55;
+            break;
+        case "bitcoin" :
+            res=cur*0.000000721;
+
     }
     document.getElementById("curesult").innerText=res;
 }
